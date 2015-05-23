2567fd30b2e27ed30239363db4cddd1c956756bd
danbeck <d.danielbeck@googlemail.com>
2015-05-23 10:19:22 +0200

    Cordova: create version 4.0 and 5.0

---
+++ base/4.0/createImage.sh
@@ -0,0 +1,2 @@
+docker build -t danielbeck/cordova:4.0.0 .
+docker push danielbeck/cordova:4.0.0
---
+++ base/4.0/getCordovaVersion.sh
@@ -0,0 +1,1 @@
+docker run --rm -v /home/daniel/workspace/rackern/:/data danielbeck/cordova:4.0.0 /usr/local/bin/cordova -v
